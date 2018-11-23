a2bc6540ee5b8fdb77d29f933daa3b38f2d1c730
maletazul <luissilva@maletazul.pt>
2018-11-23 18:34:31 +0000

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -146,6 +146,18 @@
 
 #define NOFAKE_TXS_TO_TOTAL_TXS_PERCENT         10
 
+#define HF_VERSION_DYNAMIC_FEE                  4
+#define HF_VERSION_MIN_MIXIN_4                  6
+#define HF_VERSION_MIN_MIXIN_6                  7
+#define HF_VERSION_MIN_MIXIN_10                 8
+#define HF_VERSION_ENFORCE_RCT                  6
+#define HF_VERSION_PER_BYTE_FEE                 8
+
+#define PER_KB_FEE_QUANTIZATION_DECIMALS        8
+
+#define HASH_OF_HASHES_STEP                     256
+
+
 #define BULLETPROOF_MAX_OUTPUTS                 16
 
 // New constants are intended to go here
